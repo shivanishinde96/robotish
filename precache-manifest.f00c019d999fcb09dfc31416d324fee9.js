2ff5ea19e6ba0b4818147a91b2ab1f041d45c9ae
Shivani <shivanishinde02@gmail.com>
2020-07-24 19:28:12 -0700

    Updates

---
+++ precache-manifest.f00c019d999fcb09dfc31416d324fee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ddd514b4525c5419430fed3134de1f",
+    "url": "/robotish/index.html"
+  },
+  {
+    "revision": "90310eb039a2fce637aa",
+    "url": "/robotish/static/css/main.a82fb7a2.chunk.css"
+  },
+  {
+    "revision": "e3ed43393ac082b2820e",
+    "url": "/robotish/static/js/2.d070ac75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotish/static/js/2.d070ac75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90310eb039a2fce637aa",
+    "url": "/robotish/static/js/main.1506299d.chunk.js"
+  },
+  {
+    "revision": "ee920648de6a185e1ac4",
+    "url": "/robotish/static/js/runtime-main.61960fa4.js"
+  }
+]);
